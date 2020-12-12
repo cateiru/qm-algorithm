c1e4911f586a49568d742263705abfa74a8bf6c0
Yuto Watanabe <yuto.w51942@gmail.com>
2020-12-13 03:07:45 +0900

    change indent size 4 to 2

--- src/calculate.ts
+++ src/calculate.ts
@@ -1,96 +1,96 @@
 import {QMElement} from './qm'
 
 export interface Label {
-    bin: ReadonlyArray<string>
-    dec: number[]
-    oneLength: number
+  bin: ReadonlyArray<string>
+  dec: number[]
+  oneLength: number
 }
 
 export function calculate(element: ReadonlyArray<QMElement>, numberOfInputLength: number): ReadonlyArray<Label>{
-    const label: Array<Label> = []
+  const label: Array<Label> = []
 
-    element.forEach((e, i) => {
-        const oneLength = e.exportNumberOfOne()
+  element.forEach((e, i) => {
+    const oneLength = e.exportNumberOfOne()
 
-        if(oneLength !== -1){
-            label.push({
-                bin: e.exportBinOrigenData(),
-                dec: [e.exportInputNumberData()],
-                oneLength: oneLength
-            })
+    if(oneLength !== -1){
+      label.push({
+        bin: e.exportBinOrigenData(),
+        dec: [e.exportInputNumberData()],
+        oneLength: oneLength
+      })
+    }
+  })
+
+  const term = optimization(label)
+
+  console.log(term)
+
+  let requiredTermDec: number[] = []
+  const requiredItems: Label[] = []
+  const selectItems: Label[] = []
+  const multipleTerms: number[] = []
+
+  for(let i = 0; term.length > i; ++i){
+    // 必須項を求める
+    let isRequired = false
+    for(const termDec of term[i].dec){
+      let isMin = true
+      for(let j = 0; term.length > j; ++j){
+        if(j !== i && term[j].dec.includes(termDec)){
+          isMin = false
         }
+      }
+      if(isMin){
+        isRequired = true
+      }else{
+        if(!multipleTerms.includes(termDec)){
+          multipleTerms.push(termDec)
+        }
+      }
+    }
+    if(isRequired){
+      requiredItems.push(term[i])
+      requiredTermDec = requiredTermDec.concat(term[i].dec)
+    }else{
+      selectItems.push(term[i])
+    }
+  }
+
+  const needTerm = multipleTerms.filter(i =>requiredTermDec.indexOf(i) == -1);
+  const needMax = needTerm.length
+
+  const termTag: {
+    needItems: number,
+    label: Label
+  }[] = []
+
+  // 最簡形を求める
+  for(let i = 0; selectItems.length > i; ++i){
+    let count = 0
+    for(let j = 0; needMax > j; ++j){
+      if(selectItems[i].dec.includes(needTerm[j])){
+        count++
+      }
+    }
+    termTag.push({
+      needItems: count,
+      label: selectItems[i]
     })
+  }
 
-    const term = optimization(label)
+  termTag.sort().reverse()
 
-    console.log(term)
+  let weight = needMax
+  for(const element of termTag){
+    requiredItems.push(element.label)
 
-    let requiredTermDec: number[] = []
-    const requiredItems: Label[] = []
-    const selectItems: Label[] = []
-    const multipleTerms: number[] = []
-
-    for(let i = 0; term.length > i; ++i){
-        // 必須項を求める
-        let isRequired = false
-        for(const termDec of term[i].dec){
-            let isMin = true
-            for(let j = 0; term.length > j; ++j){
-                if(j !== i && term[j].dec.includes(termDec)){
-                    isMin = false
-                }
-            }
-            if(isMin){
-                isRequired = true
-            }else{
-                if(!multipleTerms.includes(termDec)){
-                    multipleTerms.push(termDec)
-                }
-            }
-        }
-        if(isRequired){
-            requiredItems.push(term[i])
-            requiredTermDec = requiredTermDec.concat(term[i].dec)
-        }else{
-            selectItems.push(term[i])
-        }
+    weight -= element.needItems
+    if(0 >= weight){
+      break
     }
+  }
 
-    const needTerm = multipleTerms.filter(i =>requiredTermDec.indexOf(i) == -1);
-    const needMax = needTerm.length
-
-    const termTag: {
-        needItems: number,
-        label: Label
-    }[] = []
-
-    // 最簡形を求める
-    for(let i = 0; selectItems.length > i; ++i){
-        let count = 0
-        for(let j = 0; needMax > j; ++j){
-            if(selectItems[i].dec.includes(needTerm[j])){
-                count++
-            }
-        }
-        termTag.push({
-            needItems: count,
-            label: selectItems[i]
-        })
-    }
-
-    termTag.sort().reverse()
-
-    let weight = needMax
-    for(const element of termTag){
-        requiredItems.push(element.label)
-
-        weight -= element.needItems
-        if(0 >= weight){
-            break
-        }
-    }
-
-    return requiredItems
+  return requiredItems
 }
 
 /**
@@ -99,55 +99,55 @@ export function calculate(element: ReadonlyArray<QMElement>, numberOfInputLength
 */
 function optimization(label: ReadonlyArray<Label>): ReadonlyArray<Label> {
 
-    if(label.length <= 0) {
-        return []
-    }
+  if(label.length <= 0) {
+    return []
+  }
 
-    const newLabel: Label[] = []
-    const mainTerm: Label[] = []
-    let flag = Array(label.length-1).fill(false)
+  const newLabel: Label[] = []
+  const mainTerm: Label[] = []
+  let flag = Array(label.length-1).fill(false)
 
-    for(let i = 0; label.length > i; ++i){
-        for(let j = i+1; label.length > j; ++j){
-            if(Math.abs(label[i].oneLength - label[j].oneLength) === 1){
-                const parentLabel = label[i].bin
-                const childLabel = label[j].bin
+  for(let i = 0; label.length > i; ++i){
+    for(let j = i+1; label.length > j; ++j){
+      if(Math.abs(label[i].oneLength - label[j].oneLength) === 1){
+        const parentLabel = label[i].bin
+        const childLabel = label[j].bin
 
-                const equalIndex = diffArray(parentLabel, childLabel)
+        const equalIndex = diffArray(parentLabel, childLabel)
 
-                if(typeof equalIndex !== 'undefined'){
-                    flag[i] = true
-                    flag[j] = true
-                    const newBin = parentLabel.slice()
-                    newBin.splice(equalIndex, 1, '-')
+        if(typeof equalIndex !== 'undefined'){
+          flag[i] = true
+          flag[j] = true
+          const newBin = parentLabel.slice()
+          newBin.splice(equalIndex, 1, '-')
 
-                    const newDec = label[i].dec.concat(label[j].dec).sort()
-                    let isDuplicate = false
+          const newDec = label[i].dec.concat(label[j].dec).sort()
+          let isDuplicate = false
 
-                    newLabel.forEach(e=>{
-                        if(e.dec.join('') === newDec.join('')){
-                            isDuplicate = true
-                        }
-                    })
-
-                    if(!isDuplicate){
-                        newLabel.push({
-                            bin: newBin,
-                            dec: newDec,
-                            oneLength: Math.min(label[i].oneLength, label[j].oneLength)
-                        })
-                    }
-                }
+          newLabel.forEach(e=>{
+            if(e.dec.join('') === newDec.join('')){
+              isDuplicate = true
             }
+          })
+
+          if(!isDuplicate){
+            newLabel.push({
+              bin: newBin,
+              dec: newDec,
+              oneLength: Math.min(label[i].oneLength, label[j].oneLength)
+            })
+          }
         }
-        if(!flag[i]){
-            mainTerm.push(label[i])
-        }
+      }
     }
+    if(!flag[i]){
+      mainTerm.push(label[i])
+    }
+  }
 
-    const term = optimization(newLabel)
+  const term = optimization(newLabel)
 
-    return mainTerm.concat(term)
+  return mainTerm.concat(term)
 }
 
 /**
@@ -156,22 +156,22 @@ function optimization(label: ReadonlyArray<Label>): ReadonlyArray<Label> {
 * @param second Array element
 */
 function diffArray(first: ReadonlyArray<string>, second: ReadonlyArray<string>): (number|undefined) {
-    if(first.length !== second.length){
-        throw new Error('引数の配列は同じ長さである必要があります。')
-    }
+  if(first.length !== second.length){
+    throw new Error('引数の配列は同じ長さである必要があります。')
+  }
 
-    let elementIndex = 0
-    let count = 0
+  let elementIndex = 0
+  let count = 0
 
-    for(let i = 0; first.length > i; ++i){
-        if(first[i] !== second[i]){
-            elementIndex = i
-            count++
-        }
+  for(let i = 0; first.length > i; ++i){
+    if(first[i] !== second[i]){
+      elementIndex = i
+      count++
     }
+  }
 
-    if(count === 1){
-        return elementIndex
-    }
-    return
+  if(count === 1){
+    return elementIndex
+  }
+  return
 }
--- src/printData.ts
+++ src/printData.ts
@@ -2,14 +2,14 @@ import {green, red, reset, white} from './colors'
 import {QMElement} from './qm'
 
 export function printTruthTable(qmElement: ReadonlyArray<QMElement>, numberOfInputLength: number){
-    const alphabets: string[] = []
-    for(let i = 0; numberOfInputLength > i; ++i){
-        alphabets.push(String.fromCharCode(65+(i%26)))
-    }
+  const alphabets: string[] = []
+  for(let i = 0; numberOfInputLength > i; ++i){
+    alphabets.push(String.fromCharCode(65+(i%26)))
+  }
 
-    console.log(white + alphabets.join(' ') + ' | output' + reset)
+  console.log(white + alphabets.join(' ') + ' | output' + reset)
 
-    qmElement.forEach((e, i) => {
-        console.log(`${green}${e.exportBinData()}${reset} | ${red}${e.exportExportData()}${reset}`)
-    })
+  qmElement.forEach((e, i) => {
+    console.log(`${green}${e.exportBinData()}${reset} | ${red}${e.exportExportData()}${reset}`)
+  })
 }
